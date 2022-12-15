4492883110141267f4fa6f180dcb0e2c8c2cabe1
Stanislav Bubenchshikov <bubenchshikov.s@chocolife.kz>
2022-12-15 14:39:50 +0600

    correct project column

--- DOW&TOD/project_dow_purchases.sql
+++ DOW&TOD/project_dow_purchases.sql
@@ -1,9 +1,11 @@
 select
-	project,
+	COALESCE(terminal_project, project) as project,
 	date_part(dayofweek, order_created_at) as num_day,
 	to_char(order_created_at, 'Day') as name_day,
 	count(order_id) as num
 from analytics.stg_choco_orders
 where order_created_at >= '2021-12-31 23:59:59'
+	and (terminal_project in ('digital_rest', 'lensmark', 'loyalty', 'ryadom')
+		or terminal_project is null)
 group by 1, 2, 3
 order by 1, 2
--- DOW&TOD/project_tod_purchases.sql
+++ DOW&TOD/project_tod_purchases.sql
@@ -1,8 +1,10 @@
 select
-	project,
+	COALESCE(terminal_project, project) as project,
 	DATEPART(HOUR, order_created_at),
 	count(order_id) as num
 from analytics.stg_choco_orders
 where order_created_at >= '2021-12-31 23:59:59'
+	and (terminal_project in ('digital_rest', 'lensmark', 'loyalty', 'ryadom')
+		or terminal_project is null)
 group by 1, 2
-order by 1
+order by 1, 2
